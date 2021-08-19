8ece9d0d52d079016cf7318c6044313364810422
Deepak Kumar Sahoo <deepak.kumar.sahoo@ibm.com>
2021-08-19 20:08:47 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { UtilsModule } from './utils/utils.module';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BugsComponent } from './bugs/bugs.component';
+import { ClosedCountPipe } from './bugs/pipes/closed-count.pipe';
+import { BugStatsComponent } from './bugs/components/bug-stats/bug-stats.component';
+import { BugEditComponent } from './bugs/components/bug-edit/bug-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BugsComponent,
+    ClosedCountPipe,
+    BugStatsComponent,
+    BugEditComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UtilsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
